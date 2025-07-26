74eee3f0e113e23cf84b663f931c734a2754db83
Kiran Rathod <kiranrathod4299@gmail.com>
2025-07-26 20:32:59 +0000

    NEXT_PUBLIC_FIREBASE_API_KEY where i can get these keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "anna-daata-s3a55",
-  "appId": "1:85928821034:web:a3c46f7fa0df582dad1e53",
-  "storageBucket": "anna-daata-s3a55.firebasestorage.app",
-  "apiKey": "AIzaSyCxp3XknJHlZLThqIUTu5ctjH9DlQreP-I",
-  "authDomain": "anna-daata-s3a55.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "85928821034"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
